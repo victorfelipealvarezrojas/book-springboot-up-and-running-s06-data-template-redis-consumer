e27526762099f17b86c3ae529e67d89c993620cb
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2022-03-24 18:24:01 -0300

    consumo api y almaceno su respuesta en redis, libro despring cap 6 almacenamiento

---
+++ src/test/java/com/example/planefinderconsumer/PlanefinderconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planefinderconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanefinderconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
